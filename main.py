6e0a30124db044129c65b482e3451312bd19dced
decorez <decoremain@gmail.com>
2023-10-25 14:06:44 +0700

    adding folder and fonts screen

--- main.py
+++ main.py
@@ -1,4 +1,44 @@
-from kivy.uix.screenmanager import ScreenManager
-from kivy.lang import Builder
-from kivymd.uix.screen import MDScreen
-from kivymd.app import MDApp
+import os
+import importlib
+
+from kivy.core.window import Window
+
+from kaki.app import App
+
+class WorkoutApp(App):
+    KV_FILES = {
+        os.path.join(os.getcwd(),
+                    'screens', 
+                    'rootscreen',
+                    'root_screen.kv'),
+        os.path.join(os.getcwd(),
+                    'screens', 
+                    'todayworkout',
+                    'today_workout.kv'),
+        os.path.join(os.getcwd(),
+                    'screens', 
+                    'allworkout',
+                    'all_workout.kv'),
+    },
+    CLASSES = {
+        'rootscreen' : 'root_screen',
+        'todayworkout' : 'today_workout',
+        'allworkout' : 'all_workout',
+    }
+    AUTORELOADER_PATHS = [
+        ('.', {'recursive': True}),
+    ]
+
+    def build_app(self):
+        import screens.rootscreen.root_screen
+
+        Window.bind(on_keyboard=self.rebuild)
+        importlib.reload(screens.rootscreen.root_screen)
+
+        return screens.rootscreen.root_screen.RootScreen()
+    
+    def rebuild(self, *args):
+        if args[1] == 32:
+            self.rebuild()
+
+WorkoutApp().run()
